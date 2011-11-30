53ca558d11a9b9f3bb90f8f6f1075ae993e64e56
ryoishibashi <ryo500209@gmail.com>
2011-11-30 16:14:30 +0900

    map速度比較

--- JavaVerifying/src/ryo/Main.java
+++ JavaVerifying/src/ryo/Main.java
@@ -1,6 +1,11 @@
 package ryo;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -47,5 +52,31 @@ public class Main {
         System.out.println("Long max is " + longMax);
 
         BigDecimal bd2 = new BigDecimal("a");
+    }
+
+    private static void compareTime() {
+        Integer SIZE = 1000000;
+        // リストとMapを準備
+        List<Integer> list = new ArrayList<Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < SIZE; i++) {
+            list.add(i, i);
+            map.put(i, i);
+        }
+
+        Date start = new Date();
+        Integer searchKey = SIZE - 1;
+        for (Integer data : list) {
+            if (searchKey.equals(data)) {
+                Date found = new Date();
+                System.out.println(found.getTime() - start.getTime());
+                break;
+            }
+        }
+
+        Date startMap = new Date();
+        Integer value = map.get(searchKey);
+        Date foundMap = new Date();
+        System.out.println(foundMap.getTime() - startMap.getTime());
     }
 }
